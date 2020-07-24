fa9f3dae301d64e0acf6d13572c85f37d0077040
jareiner90 <johnreiner1990@gmail.com>
2020-07-24 09:41:21 -0400

    learned about yargs, a npm package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,59 @@
-const validator = require('validator')
-getNotes = require('./notes.js')
+const getNotes = require('./notes.js')
+const chalk = require('chalk')
+const yargs = require('yargs');
 
-notes = getNotes('whatever I want...')
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(notes)
+// Create add command 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ', argv.title)
+        console.log('Body: ', argv.body)
+    }
+})
 
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function (argv) {
+        console.log('Removing the note', argv)
+    }
+})
 
-console.log(validator.isURL('https/johnreiner.com'))
+yargs.command({
+    command: 'list',
+    describe: 'List out all of your notes',
+    handler: function () {
+        console.log('Listing our your notes now...')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'View a note...',
+    handler: function () {
+        console.log('This is your note...')
+    }
+})
+
+// add, remove, read, list
+
+// console.log(process.argv)
+// console.log(yargs.argv)
+yargs.parse()
