7da684c4b53da3b8cf5915659d2e881085acf44a
jareiner90 <johnreiner1990@gmail.com>
2020-07-24 11:22:08 -0400

    adding to the note taking app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,4 @@
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 const chalk = require('chalk')
 const yargs = require('yargs');
 
@@ -22,8 +22,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Title: ', argv.title)
-        console.log('Body: ', argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
@@ -31,8 +30,15 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
     handler: function (argv) {
-        console.log('Removing the note', argv)
+        notes.removeNote(argv.title)
     }
 })
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,67 @@
-const getNotes = (notes) => {
-    return notes
+const fs = require ('fs')
+const chalk = require('chalk')
+
+
+
+const getNotes = () => {
+    return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNote = (title, body) => {
+    const notes = loadNotes()
+
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title 
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })     
+        console.log('New note added')   
+    } else {
+        console.log('Note title taken!')
+    }
+
+
+
+    saveNotes(notes)
+
+}
+
+const removeNote = title => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(note => note.title !== title)
+    
+    if (notesToKeep.length === notes.length) {
+        console.log(chalk.bgRed(`The note title ${chalk.underline(title)} does not exist...`))
+    } else {
+        saveNotes(notesToKeep)
+        console.log(chalk.bgGreen(`You have deleted the ${chalk.underline(title)} note!`))
+    }
+    
+}
+
+const saveNotes = notes => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (error) {
+        return []
+    }
+
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+}
